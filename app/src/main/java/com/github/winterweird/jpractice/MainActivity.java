9b5ff3eabcac6b60b3dc3e252793ac669558e637
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-04-23 22:33:25 +0200

    Close #6 - add MainActivity JavaDoc

--- app/src/main/java/com/github/winterweird/jpractice/MainActivity.java
+++ app/src/main/java/com/github/winterweird/jpractice/MainActivity.java
@@ -12,8 +12,21 @@ import android.content.Context;
 import com.github.winterweird.jpractice.database.DatabaseHelper;
 import com.github.winterweird.jpractice.database.data.Entry;
 
+/**
+ * The main activity of the application.
+ *
+ * This is the activity that gets called on startup, and is the first screen the
+ * user of the application will see.
+ */
 public class MainActivity extends AppCompatActivity {
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     *
+     * Sets they layout, the support toolbar, and sets up navigation listeners
+     * for each of the buttons which start child activities.
+     *
+     * @param savedInstanceState The saved instance state
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +44,12 @@ public class MainActivity extends AppCompatActivity {
         navigateListener(findBtn, FindWordsActivity.class);
     }
 
-    // Helper method to avoid unnecessary repetition
+    /**
+     * Helper method: set the click listener of a navigation button.
+     *
+     * @param btn The button to add a listener to
+     * @param cls The activity class to open when the button is clicked
+     */
     private void navigateListener(Button btn, Class<?> cls) {
         final Class<?> clsFinal = cls;
         btn.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +61,18 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // debug tool to fix parts of the database which accidentally break
+    /**
+     * Helper method: use to fix a broken entry while debugging.
+     *
+     * Sometimes something that is not properly implemented or something that
+     * I'm experimenting with can put an entry in a broken state. This method is
+     * here to fix broken entries. It's particularly useful in the case when one
+     * entry accidentally gets stuck with position -1, which has happened at
+     * least once.
+     *
+     * @param broken The entry that is broken
+     * @param fixed What the fixed entry looks like
+     */
     private void fix(Entry broken, Entry fixed) {
         DatabaseHelper dbhelper = DatabaseHelper.getHelper(this);
         dbhelper.update(broken, fixed);
