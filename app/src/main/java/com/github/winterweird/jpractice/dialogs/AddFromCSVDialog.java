da759139c1daa9300c0ce70aaa06d8107286434d
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-04-27 11:05:49 +0200

    Add AddFromCSVDialog layout skeleton
    Added the components required for the interface, at least mostly, to XML and
    retrieved them using Java.

--- app/src/main/java/com/github/winterweird/jpractice/dialogs/AddFromCSVDialog.java
+++ app/src/main/java/com/github/winterweird/jpractice/dialogs/AddFromCSVDialog.java
@@ -9,6 +9,8 @@ import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.app.Activity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
 import com.github.winterweird.jpractice.R;
 
@@ -16,21 +18,30 @@ public class AddFromCSVDialog extends DialogFragment {
     private String title = "Add data from CSV";
     private String confirmText = "Add data";
     private String cancelText = "Cancel";
-    private DialogInterface.OnClickListener positiveClickListener;
-
-    public AddFromCSVDialog() {
-        this.positiveClickListener = (dialog, id) -> addDataToDatabase();
-    }
+    private boolean pasteMode = true;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Activity act = getActivity();
         LayoutInflater inflater = act.getLayoutInflater();
+        
         final View v = inflater.inflate(R.layout.add_from_csv_dialog, null);
+        
+        // View children
+        final EditText pathSelection = v.findViewById(R.id.addFromCSVPathSelection);
+        final EditText pasteBox = v.findViewById(R.id.addFromCSVPasteBox);
+        final Button changeMode = v.findViewById(R.id.addFromCSVChangeModeButton);
+        
         return new AlertDialog.Builder(getActivity(), R.style.AlertDialogTheme)
             .setTitle(title)
             .setView(v)
-            .setPositiveButton(confirmText, positiveClickListener)
+            .setPositiveButton(confirmText, (dialog, id) -> {
+                String s;
+                if (pasteMode) s = pasteBox.getText().toString();
+                else           s = pathSelection.getText().toString();
+                // TODO: use s
+                Log.d("Test", "Text is: " + s);
+            })
             .setNegativeButton(cancelText, (dialog, id) -> {})
             .create();
     }
