05b2ddb4ad12b869807e52a7779d66c249b15d98
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2019-01-25 14:59:36 +0100

    Add documentation to FilePicker and FilePickerActivity

--- app/src/main/java/com/github/winterweird/jpractice/util/FilePicker.java
+++ app/src/main/java/com/github/winterweird/jpractice/util/FilePicker.java
@@ -8,11 +8,26 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.app.ActivityCompat;
 import android.net.Uri;
 
+/**
+ * Utility class which can be used to find a file with a given callback once the
+ * file is found.
+ */
 public class FilePicker {
-    private Activity context;
-    private static FilePicker fp;
+    private Activity context; // the context for this specific file picker
+    private static FilePicker fp; // a static file picker that may be trouble
+    
+    // this allows us to keep track of the continuation we intend to execute
+    // once the file is found, and call it in the actual activity which handles
+    // the result
     private static Callback mostRecentCallback;
 
+    /**
+     * Get a previously instantiated FilePicker instance.
+     *
+     * @return the previously constructed instance FilePicker instance
+     * @throws IllegalStateException if called without the FilePicker being
+     * instantiated
+     */
     public static FilePicker getInstance() {
         if (fp == null) {
             throw new IllegalStateException("FilePicker instance context not set");
@@ -20,6 +35,21 @@ public class FilePicker {
         return fp;
     }
     
+    /**
+     * Get a FilePicker instance.
+     *
+     * This method must be called once with arguments to "prime" the FilePicker
+     * instance.
+     *
+     * NOTE: Potential problem I see just from reading this is that there may be a
+     * situation where the filepicker is instantiated with a context that has
+     * expired. Should probably fix that.
+     *
+     * @param context The context to instantiate the FilePicker with (only
+     * applicable on first call)
+     * @return a new instance if none exists, otherwise return the previously
+     * constructed instance.
+     */
     public static FilePicker getInstance(Activity context) {
         if (fp == null) {
             fp = new FilePicker(context);
@@ -27,17 +57,34 @@ public class FilePicker {
         return fp;
     }
     
+    /**
+     * Private constructor.
+     */
     private FilePicker(Activity context) {
         this.context = context;
     }
 
+    /**
+     * Find a file and specify the callback that should be executed given the
+     * data once the file is found.
+     *
+     * @param callback The callback to be executed once the resource has been
+     * procured
+     */
     public void findFile(Callback callback) {
         mostRecentCallback = callback;
         Intent intent = new Intent(context, FilePickerActivity.class);
         context.startActivity(intent);
     }
 
+    /**
+     * Interface specifying a callback given a file Uri.
+     */
     public static interface Callback {
+        /**
+         * The callback to be executed.
+         * @param filename The Uri of the file resource to open
+         */
         void callback(Uri filename);
     }
 
--- app/src/main/java/com/github/winterweird/jpractice/util/FilePickerActivity.java
+++ app/src/main/java/com/github/winterweird/jpractice/util/FilePickerActivity.java
@@ -7,26 +7,65 @@ import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.app.ActivityCompat;
 
+/**
+ * An activity which may be started by the FilePicker util class in order to
+ * retrieve a result file from a file dialog and call the callback on the given
+ * result file.
+ */
 public class FilePickerActivity extends Activity {
+    // NOTE: These do not refer to the same category of things, so they can have
+    // identical numbers
     public static final int PERMISSION_READ_EXTERNAL_STORAGE = 1;
     public static final int READ_REQUEST_CODE = 1;
     
+    /**
+     * Requests permissions to read external storage upon creation.
+     *
+     * @param savedInstanceState The previously saved instance
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // get permission to read external storage, or get previous answer to
+        // request
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 
                 PERMISSION_READ_EXTERNAL_STORAGE); 
         
     }
 
+    /**
+     * After permissions have been granted, open a file picker dialog that
+     * allows the user to select a document.
+     *
+     * NOTE: Does not check actual permissions(?) TODO: Test
+     *
+     * @param rc The request code for the type of permissions we are requesting
+     * @param permissions The requested permissions
+     * @param res The results of the permission requests
+     * (PERMISSION_GRANTED|PERMISSION_DENIED) (not checked)
+     */
     @Override
     public void onRequestPermissionsResult(int rc, String permissions[], int[] res) {
+        // open any document; we're not picky
+        // NOTE: In practice this will be CSV data
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.setType("*/*");
+        // request code in return result identifies the kind of request the
+        // result is an answer to
         this.startActivityForResult(intent, READ_REQUEST_CODE);
     }
     
+    /**
+     * Once the result of the open document action has been determined, if it is
+     * a success, execute the callback specified when calling
+     * FilePicker.findFile().
+     *
+     * @param requestCode The request code for the result we wanted to get
+     * @param resultCode The result code returned by the child activity using
+     * setResult
+     * @param data The result data (specified by the child activity)
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Check which request we're responding to
